c6127177b2b571c8c937426da0fbd54d104a944f
MrCubee <contact@mrcubee.fr>
2024-06-02 01:13:53 +0200

    Add repository.

---
+++ src/main/java/fr/belouga/SpeedRunPlugin.java
@@ -0,0 +1,22 @@
+package fr.belouga;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpeedRunPlugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
