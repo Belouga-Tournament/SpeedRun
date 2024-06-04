c2d41dd0623a15b41df7880c987ba2e711ddd4ee
NickNqck <jadorevolvic@gmail.com>
2024-06-04 14:07:11 +0200

    Ajout en cours d'un scoreboard

--- spigot/src/main/java/fr/belouga/listener/PlayerEvent.java
+++ spigot/src/main/java/fr/belouga/listener/PlayerEvent.java
@@ -1,14 +1,25 @@
 package fr.belouga.listener;
 
+import fr.belouga.spigot.SpeedRunPlugin;
+import fr.mrmicky.fastboard.FastBoard;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerEvent implements Listener {
 
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent e){
+        FastBoard board = new FastBoard(e.getPlayer());
+        board.updateTitle("§cTest");//Je définie le nom du scoreboard
+        SpeedRunPlugin.getInstance().getBoards().put(e.getPlayer().getUniqueId(), board);
         e.setJoinMessage("§8[§a+1§8]§f [RANK] "+e.getPlayer().getDisplayName());
     }
-
+    @EventHandler
+    private void onPlayerLeave(PlayerQuitEvent e){
+        FastBoard board = SpeedRunPlugin.getInstance().getBoards().remove(e.getPlayer().getUniqueId());
+        assert board != null;
+        board.delete();
+    }
 }
--- spigot/src/main/java/fr/belouga/spigot/SpeedRunPlugin.java
+++ spigot/src/main/java/fr/belouga/spigot/SpeedRunPlugin.java
@@ -2,9 +2,20 @@ package fr.belouga.spigot;
 
 import fr.belouga.listener.PlayerEvent;
 import fr.belouga.listener.RegisterListeners;
+import fr.mrmicky.fastboard.FastBoard;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class SpeedRunPlugin extends JavaPlugin {
+
+    @Getter
+    private static SpeedRunPlugin instance;
+    @Getter
+    private final Map<UUID, FastBoard> boards = new HashMap<>();
 
     @Override
     public void onLoad() {
@@ -13,8 +24,14 @@ public class SpeedRunPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        instance = this;
         RegisterListeners.register(this);
         registerListeners();
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (FastBoard board : this.boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, 20);
     }
 
     @Override
@@ -24,5 +41,11 @@ public class SpeedRunPlugin extends JavaPlugin {
     private void registerListeners(){
         getServer().getPluginManager().registerEvents(new PlayerEvent(), this);
     }
-
+    public void updateBoard(FastBoard board){
+        assert board != null;
+        board.updateLines(
+                "ligne 1",
+                "ligne 2"
+        );
+    }
 }
